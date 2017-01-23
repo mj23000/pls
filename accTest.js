78ffb608b1eea8944d66cbaa575a3931c9b50cfc
mj23000 <markusjacobsen@hotmail.com>
2017-01-23 14:17:00 +0100

    d

--- accTest.js
+++ accTest.js
@@ -1,7 +1,4 @@
 var watchID = null;
-var z=0;
-var prim=1;
-var primtal=1000;
 
 //I onLoad tilføjes eventlistener
 function onLoad(){
@@ -15,7 +12,7 @@ startWatch();
 
 //måler acceleration i bestemte intervaller
 function startWatch() {
-var options = { frequency: 10 };//vis værdi hvert sekund
+var options = { frequency: 5 };//vis værdi hvert sekund
 watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options); //
 }
 
@@ -28,36 +25,17 @@ watchID = null;
 
 function onSuccess(acceleration) {
 //Gem sensorværdier
-var accX = acceleration.x;
+var accX = acceleration.x ;
 var accY = acceleration.y;
 var accZ = acceleration.z;
 var timestamp = acceleration.timestamp;
-var primtal = accX*37;
 //Udskriv værdier i div med navnet accelerometer
-for(var j=1;j<=primtal;j++){
-		prim=1;	
-		for(var i=2;i<j;i++){
-			z=j%i;
-			if (z==0)
-				prim=0;
-		}
-		if (prim==1)
-			document.getElementById("primsvar").innerHTML += j+" ";
-	}
-document.getElementById('accelerometer').innerHTML = 
-'Acceleration X: ' + accX + '<br />' +
+document.getElementById('accelerometer').innerHTML = 'Acceleration X: ' + accX + '<br/>' +
 'Acceleration Y: ' + accY + '<br />' +
 'Acceleration Z: ' + accZ + '<br />' +
-'Timestamp	   : ' + timestamp + '<br />' ;
+'Timestamp: ' + timestamp + '<br />';
 }
 
 function onError() {
 alert('onError!');
 }
-
-
-
-	
-
-
-
